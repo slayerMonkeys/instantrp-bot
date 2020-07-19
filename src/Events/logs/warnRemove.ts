3ae333f4ffc7838d3ef05c6e5c2ad08d62c6e2ea
Slayer <gori.mods@gmail.com>
2020-07-19 16:55:57 +0200

    add event warnRemove

---
+++ src/Events/logs/warnRemove.ts
@@ -0,0 +1,28 @@
+import { Message, GuildMember, MessageEmbed, TextChannel } from "discord.js"
+import { getChannelLog } from "../../services/guildSettingsService"
+
+module.exports = async (client: any, message: Message, member: GuildMember, warn: any, numero: number) => {
+    const channelLogId = await getChannelLog(client.sequelize, message.guild, 'misc')
+    const channel: TextChannel = client.channels.cache.find((c) => c.id === channelLogId)
+    const embed = new MessageEmbed()
+    .setColor(2240347)
+    .setTimestamp(new Date())
+    .setFooter('Warn LOG', client.user.displayAvatarURL())
+    .setThumbnail(member.user.displayAvatarURL())
+    .setAuthor('Warn LOG', message.guild.iconURL())
+    .addFields([
+      {
+        name: "Personne qui a éxecuté la commande",
+        value: `${message.author}`,
+      },
+      {
+        name: "Utilisateur qui avait le warn",
+        value: `${member}`,
+      },
+      {
+        name: "info du warn",
+        value: `Raison: ${warn.rows.reason}\n Numéro: ${numero}`,
+      },
+    ])
+    channel.send(embed)
+}
