38bc87037386b9f1a5daa4ca5f9808361ddb8dee
Slayer <gori.mods@gmail.com>
2020-07-19 16:55:31 +0200

    add event warnAdd

---
+++ src/Events/logs/warnAdd.ts
@@ -0,0 +1,28 @@
+import { Message, GuildMember, MessageEmbed, TextChannel } from "discord.js"
+import { getChannelLog } from "../../services/guildSettingsService"
+
+module.exports = async (client: any, message: Message, member: GuildMember, reason: string) => {
+    const channelLogId = await getChannelLog(client.sequelize, message.guild, 'misc')
+    const channel: TextChannel = client.channels.cache.find((c) => c.id === channelLogId)
+    const embed = new MessageEmbed()
+    .setColor(2240347)
+    .setTimestamp(new Date())
+    .setFooter('Warn LOG', client.user.displayAvatarURL())
+    .setThumbnail(member.user.displayAvatarURL())
+    .setAuthor('Warn LOG', message.guild.iconURL())
+    .addFields([
+      {
+        name: "Personne qui a éxecuté la commande",
+        value: `${message.author}`,
+      },
+      {
+        name: "Utilisateur Warn",
+        value: `${member}`,
+      },
+      {
+        name: "Raison",
+        value: `${reason}`,
+      },
+    ])
+    channel.send(embed)
+}
