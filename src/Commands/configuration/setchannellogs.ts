4b7acd57db829fb73f63b4461a615eb65c7f7704
Slayer <gori.mods@gmail.com>
2020-08-15 16:30:58 +0200

    add command setChannelLogs

--- src/Commands/configuration/setchannellogs.ts
+++ src/Commands/configuration/setchannellogs.ts
@@ -1,19 +1,18 @@
 import { Client, Message, GuildChannel } from "discord.js";
-import { IHelp } from "../../utils/interface";
+import { IHelp } from "../../typescript/interface";
 import { setLogsChannel } from "../../services/guildSettingsService";
 
 module.exports.run = async (client: any, message: Message, args: string[]) => {
     const channel: GuildChannel = message.mentions.channels.first() || message.guild.channels.cache.find((c: GuildChannel) => c.id === args[1]);
     if(!channel) return message.reply('channel invalid !');
-    const types: string[] = ['']
     const cb = await setLogsChannel(client.sequelize, message.guild, args[0], channel.id)
 };
 
 const help: IHelp = {
     name: "setchannellogs",
-    aliases: ["setchannellogs"],
-    category: "dev",
-    permLevel: "owner",
-    ownercommand: true
+    description: 'Permet de définire les channels de logs',
+    category: "Système",
+    permLevel: "Fonda",
+    usages: ['setchannellogs <typeofLog> <ChannelID>']
   };
 module.exports.help = help;
