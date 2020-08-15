36cdd2fa2b27efa1ca39efc34059f3aa0666c84d
Slayer <gori.mods@gmail.com>
2020-08-15 16:37:29 +0200

    update command kick

--- src/Commands/moderation/kick.ts
+++ src/Commands/moderation/kick.ts
@@ -1,4 +1,4 @@
-import { IHelp } from "../../utils/interface";
+import {IHelp} from "../../typescript/interface";
 
 module.exports.run = (client, message, args) => {
     const kickMember =  message.guild.member(message.mentions.users.first());
@@ -7,44 +7,16 @@ module.exports.run = (client, message, args) => {
     const staffRole = message.guild.roles.cache.find(r => r.id === '716019522183757944');
     if (!staffRole) return message.reply('Error !')
     if (kickMember.roles.cache.has(staffRole.id)) return message.reply('Vous ne pouvez pas kick un membre du staff')
-
-    /*const embed = {
-      color: 2240347,
-      timestamp: `${Date.now()}`,
-      footer: {
-        icon_url: `${message.guild.iconURL()}`,
-        text: "kick LOG",
-      },
-      thumbnail: {
-        url: `${message.author.avatarURL()}`,
-      },
-      author: {
-        name: "kick LOG",
-        icon_url: `${message.guild.iconURL()}`,
-      },
-      fields: [
-        {
-          name: "Personne qui a éxecuté la commande",
-          value: `${message.author}`,
-        },
-        {
-          name: "Personne kick",
-          value: `${user}`,
-        },
-        {
-          name: "Raisons",
-          value: `${reason}`,
-        },
-      ],
-    };*/
+    kickMember.kick(reason)
+    client.emit('guildMemberKick', kickMember, reason);
 };
 
 
 const help: IHelp = {
     name: "kick",
-    aliases: ["kick"],
+    description: 'Kick la personne mentionné',
     category: "modération",
     permLevel: "Modo",
-    ownercommand: false
+    usages: ['kick <Mention> <Raison>']
 };
 module.exports.help = help;
